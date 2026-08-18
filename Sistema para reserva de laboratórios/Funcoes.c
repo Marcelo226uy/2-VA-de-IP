91f1a61f55357d61a2e11a5d57f50d9a4c34e912
MarceloFilho22 <filhomarcelo397@gmail.com>
2026-08-18 23:31:13 +0000

    Teste

--- Sistema para reserva de laboratórios/Funcoes.c
+++ Sistema para reserva de laboratórios/Funcoes.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 #include "Funcoes.h"
+
+void cadastrarReserva(Data *prt){
+    printf("Defina o dia, mês e ano para sua reserva.\n")
+    scanf("%d %d %d", &prt->dia, &prt->mes, &&prt->ano);
+    
+}
+
+
+
